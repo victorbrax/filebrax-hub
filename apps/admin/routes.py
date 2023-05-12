b14e6c70df472b9b8e1f3b848fb45891c6b22f3f
Víctor Gomes <joaogomespcl@gmail.com>
2023-05-12 01:03:40 -0300

    nova mecanica, paginas e funcoes

--- apps/admin/routes.py
+++ apps/admin/routes.py
@@ -6,11 +6,12 @@ from utils.format_functions import *
 menu = Blueprint('menu', __name__, static_folder="static", template_folder="templates")
 
 load_dotenv()
+UPLOADS_DIR = os.getenv("UPLOADS_DIR")
 DOWNLOADS_DIR = os.getenv("DOWNLOADS_DIR")
 
-# # Página Menu de Projetos
-@menu.route('/')
-def menu_page():
+# # Página Menu de Downloads
+@menu.route('/downloads')
+def downloads_menu():
     menu_info = []
 
     # Loop que fornece as informações pra tabela do Diretório Projetos
@@ -23,6 +24,25 @@ def menu_page():
             "modified": os.path.getmtime(download_path)
         })
 
-    return render_template('admin/menu.html', projects=menu_info,
+    return render_template('admin/download_menu.html', projects=menu_info,
+                           format_size=format_size,
+                           format_date=format_date)
+
+# # Página Menu de Projetos
+@menu.route('/uploads')
+def uploads_menu():
+    menu_info = []
+
+    # Loop que fornece as informações pra tabela do Diretório Projetos
+    for report in os.listdir(UPLOADS_DIR):
+        download_path = os.path.join(UPLOADS_DIR, report)
+        size = get_size(download_path)
+        menu_info.append({
+            "name": report,
+            "size": size,
+            "modified": os.path.getmtime(download_path)
+        })
+
+    return render_template('admin/uploads_menu.html', projects=menu_info,
                            format_size=format_size,
                            format_date=format_date)
